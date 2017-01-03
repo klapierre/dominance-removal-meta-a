91d26a1756a0b0de0abd03e2c8d25a1918e6a06b
klapierre <lapierre.kimberly@gmail.com>
2017-01-03 14:37:19 -0500

    Cleaned up code: removed hedges' d calculations and responses, because they don't include datasets with no variance measure, and reordered panels in figures.

--- dominance_removals.R
+++ dominance_removals.R
@@ -41,12 +41,8 @@ barGraphStats <- function(data, variable, byFactorNames) {
 removals <- read.csv('removal data_11032016.csv')%>%
   #calculate lrr
   mutate(lrr=log(treatment_mean/control_mean))%>%
-  #calculate hedges' d for studies with variance
-  mutate(hedges_d=((treatment_mean-control_mean)/sqrt(((treatment_number-1)*treatment_variance+(control_number-1)*control_variance)/(treatment_number+control_number-2)))*(1-(3/(4*(control_number+treatment_number-2)-1))))%>%
   #calculate abs value of lrr
-  mutate(abs_lrr=abs(lrr))%>%
-  #calculate abs value of hedges' d
-  mutate(abs_hedges_d=abs(hedges_d))
+  mutate(abs_lrr=abs(lrr))
   
 #split into community, ecosystem, and competition responses
 ecosystem <- subset(removals, response_recategorized=='ecosystem')%>%
@@ -55,75 +51,60 @@ community <- subset(removals, response_recategorized=='community')%>%
   mutate(response_variable_code=ifelse(response_variable=='richness', 'richness', 'other'))
 competition <- subset(removals, response_recategorized=='competition')
 
+
+#######################
+###directional responses
+
 #mixed effects models
 summary(lrrModel <- lme(lrr ~ pulse.press + response_recategorized + study_months + study_system_detail, random=~1|NUM, data=removals))
-summary(hedgesModel <- lme(hedges_d ~ pulse.press + response_recategorized + study_months + study_system_detail, random=~1|NUM, data=subset(removals, !is.na(hedges_d))))
 
 ###ttests - lnRR
-# ecosystemProductivity <- subset(ecosystem, response_variable=='productivity')
-# ecosystemOther <- subset(ecosystem, response_variable!='productivity')
-# t.test(ecosystemProductivity$lrr, alternative='t', conf.level=0.95)
-# t.test(ecosystemOther$lrr, alternative='t', conf.level=0.95)
-
-
 t.test(ecosystem$lrr, alternative='t', conf.level=0.983334) #bonferroni corrected conf.level for 3 tests
-
 t.test(community$lrr, alternative='t', conf.level=0.983334)
-
 t.test(competition$lrr, alternative='t', conf.level=0.983334)
+#ecosystem, competition, and community are not significantly diff from 0
 
-
-###ttests - hedges' d
-t.test(ecosystem$hedges_d, alternative='t', conf.level=0.983334)
-
-t.test(community$hedges_d, alternative='t', conf.level=0.983334)
-
-t.test(competition$hedges_d, alternative='t', conf.level=0.983334)
-
-
-
-###plot - lnRR
-ggplot(removals, aes(x=response_recategorized, y=lrr)) +
-  geom_boxplot() +
-  xlab('') +
-  ylab('ln Response Ratio') +
-  scale_y_continuous(breaks=seq(-4,8,2)) +
-  scale_x_discrete(labels=c('community\n(45)', 'competition\n(12)', 'function\n(49)'))
-#export at 900x700
-
-#hedges' d
-ggplot(removals, aes(x=response_recategorized, y=hedges_d)) +
-  geom_boxplot() +
-  xlab('') +
-  ylab('Hedges d') +
-  # scale_y_continuous(breaks=seq(-4,8,2)) +
-  scale_x_discrete(labels=c('community\n(29)', 'competition\n(11)', 'function\n(38)'))
 
 
 #######################
-
 ###abs value of lrr
 
 #mixed effects models
 summary(lrrModel <- lme(abs_lrr ~ pulse.press + response_recategorized + study_months + study_system_detail, random=~1|NUM, data=removals))
-summary(hedgesModel <- lme(abs_hedges_d ~ pulse.press + response_recategorized + study_months + study_system_detail, random=~1|NUM, data=subset(removals, !is.na(abs_hedges_d))))
 
-###ttests - lnRR
+###ttests - abs lnRR
 t.test(ecosystem$abs_lrr, alternative='t', conf.level=0.983334)
-
 t.test(community$abs_lrr, alternative='t', conf.level=0.983334)
-
 t.test(competition$abs_lrr, alternative='t', conf.level=0.983334)
-#ecosystem, community, and competition are all significantly diff from 0 when abs value
+#ecosystem and community are significantly diff from 0
 
-###plot - lnRR
+
+
+#######################
+###figures
+#abs lnRR
 absLnRRPlot <- ggplot(removals, aes(x=response_recategorized, y=abs_lrr)) +
   geom_boxplot() +
   xlab('') +
-  ylab('|ln Response Ratio|') +
-  # scale_y_continuous(breaks=seq(-4,8,2)) +
-  scale_x_discrete(labels=c('community\n(45)', 'competition\n(12)', 'function\n(49)')) +
+  ylab('|lnRR|') +
+  scale_y_continuous(breaks=seq(-4,8,2)) +
+  coord_cartesian(ylim=c(0,8), xlim=c(1,3)) +
+  scale_x_discrete(labels=c('', '', '')) +
   annotate('text', x=1.1, y=0.8, label='*', size=10, hjust='left') +
   annotate('text', x=3.1, y=0.8, label='*', size=10, hjust='left') +
-annotate('text', x=0, y=7, label='(a)', size=10, hjust='left')
-#export at 900x700
+  annotate('text', x=0.6, y=8, label='(a)', size=10, hjust='left')
+
+#lnRR
+lnRRPlot <- ggplot(removals, aes(x=response_recategorized, y=lrr)) +
+  geom_boxplot() +
+  xlab('') +
+  ylab('lnRR') +
+  scale_y_continuous(breaks=seq(-4,8,2)) +
+  coord_cartesian(ylim=c(-4,8), xlim=c(1,3)) +
+  scale_x_discrete(labels=c('community\n(45)', 'competition\n(12)', 'function\n(49)')) +
+  annotate('text', x=0.6, y=8, label='(b)', size=10, hjust='left')
+
+pushViewport(viewport(layout=grid.layout(2,1)))
+print(absLnRRPlot, vp=viewport(layout.pos.row = 1, layout.pos.col = 1))
+print(lnRRPlot, vp=viewport(layout.pos.row = 2, layout.pos.col = 1))
+#export at 900x1400
